f746804ba83d679cde0486ec0d82c18610cff7ca
joaquinuc150 <surrenderpls2001@gmail.com>
2022-11-22 11:30:55 -0300

    add icons in navbar

--- src/components/nav_bar.jsx
+++ src/components/nav_bar.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import EventIcon from '@mui/icons-material/Event';
+import FamilyRestroomIcon from '@mui/icons-material/FamilyRestroom';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 export const NavBar = () => {
   return (
@@ -8,13 +11,22 @@ export const NavBar = () => {
         className={({ isActive }) => `nav-bar__link ${isActive ? 'nav-bar__link--active' : ''}`}
         to='/'
       >
-        Página de inicio
+        <EventIcon />
+        Reserva de hora
       </NavLink>
       <NavLink
         className={({ isActive }) => `nav-bar__link ${isActive ? 'nav-bar__link--active' : ''}`}
         to='/internal'
       >
-        Página Interna
+        <FamilyRestroomIcon />
+        Grupo familiar
+      </NavLink>
+      <NavLink
+        className={({ isActive }) => `nav-bar__link ${isActive ? 'nav-bar__link--active' : ''}`}
+        to='/perfil'
+      >
+        <AccountCircleIcon />
+        Perfil
       </NavLink>
     </nav>
   )
