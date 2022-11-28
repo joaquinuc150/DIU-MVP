5ba47220e1ea2ea2480d06eac91f058e84b7276b
joaquinuc150 <joaquinuc.gatica@sansano.usm.cl>
2022-11-27 23:10:45 -0300

    edit function

--- src/components/profiles.jsx
+++ src/components/profiles.jsx
@@ -7,7 +7,41 @@ import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import Avatar from '@mui/material/Avatar';
 import Link from '@mui/material/Link';
+import CheckIcon from '@mui/icons-material/Check';
 import ModeEditOutlineIcon from '@mui/icons-material/ModeEditOutline';
+
+export const EditInfo = ({data}) => {
+  const handleInput = () => { 
+    setEdit(<input ref={inputRef} type="text " />)
+    setButton(<IconButton color="primary" component="label" onClick={handleSubmit}>
+    <CheckIcon />
+    </IconButton>)
+  }
+  
+  const handleSubmit = () => {
+    setEdit(<Link href="#">{inputRef.current.value}</Link>)
+    setButton(<IconButton color="primary" component="label" onClick={handleInput}>
+    <ModeEditOutlineIcon />
+    </IconButton>)
+  }
+
+  const inputRef = React.useRef(null)
+  const [edit, setEdit] = React.useState(<Link href="#">{data}</Link>)
+  const [button, setButton] = React.useState(
+  <IconButton color="primary" component="label" onClick={handleInput}>
+  <ModeEditOutlineIcon />
+  </IconButton>
+  )
+
+
+  return (
+      <Typography component="div" variant="p">
+          {edit}
+          {button}
+      </Typography>
+  )
+}
+
 
 export const Profiles = ({name, family, edad, email, tel, image}) => {
   const theme = useTheme();
@@ -25,23 +59,11 @@ export const Profiles = ({name, family, edad, email, tel, image}) => {
           <Typography component="div" variant="p">
            {family}
           </Typography>
-          <Typography component="div" variant="p">
+          <Typography component="div" variant="p" marginBottom='20px'>
            {edad} años
           </Typography>
-          <Typography component="div" variant="p" sx={{ marginTop: '10px'}}>
-          <Link href="#">{email}</Link>
-          <IconButton color="primary" aria-label="upload picture" component="label">
-            <input hidden type="email"/>
-            <ModeEditOutlineIcon />
-          </IconButton>
-          </Typography>
-          <Typography component="div" variant="p">
-          <Link href="#">{tel}</Link>
-          <IconButton color="primary" component="label">
-            <input hidden type="tel"/>
-            <ModeEditOutlineIcon />
-          </IconButton>
-          </Typography>
+          <EditInfo data={email} />
+          <EditInfo data={tel} />
           
         </CardContent>
         <Box sx={{ display: 'flex', alignItems: 'center', pl: 1, pb: 1 }}>
